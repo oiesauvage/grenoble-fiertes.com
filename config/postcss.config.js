b1671465dc93abb13133242add48bba1704a5aea
Nils Artiges <55919407+oiesauvage@users.noreply.github.com>
2026-05-20 21:19:29 +0200

    fix: remove autoprefixer (needs read perms)

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,10 +1,8 @@
-const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss');
 // const whitelister = require('purgecss-whitelister');
 
 module.exports = {
   plugins: [
-    autoprefixer(),
     purgecss({
       content: [
         './layouts/**/*.html',
